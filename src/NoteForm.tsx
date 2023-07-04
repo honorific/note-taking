596b5648a6769a9b78bb434d072f44fc83e604a6
honorific <honorific.info@gmail.com>
2023-07-04 11:03:40 +0330

    declaring types

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,14 +1,24 @@
-import {useRef} from 'react'
+import {FormEvent, useRef} from 'react'
 import {Form, Stack, Row, Col, Button} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import CreatableReactSelect from 'react-select/creatable'
+import {NoteData} from './App'
 
-const NoteForm = () => {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void
+}
+
+const NoteForm = ({onSubmit}: NoteFormProps) => {
   const titleRef = useRef<HTMLInputElement>(null)
   const markdownRef = useRef<HTMLTextAreaElement>(null)
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
