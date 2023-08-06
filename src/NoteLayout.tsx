569235363dcfca08db8d35c17252670ad2d34fee
honorific <honorific.info@gmail.com>
2023-08-06 22:06:36 +0330

    useNote utility

--- src/NoteLayout.tsx
+++ src/NoteLayout.tsx
@@ -1,4 +1,4 @@
-import {Navigate, Outlet, useParams} from 'react-router-dom'
+import {Navigate, Outlet, useOutletContext, useParams} from 'react-router-dom'
 import {Note} from './App'
 
 type NoteLayoutProps = {
@@ -13,6 +13,10 @@ const NoteLayout = ({notes}: NoteLayoutProps) => {
     return <Navigate to='/' replace />
   }
   return <Outlet context={note} />
+}
+
+export const useNote = () => {
+  return useOutletContext<Note>()
 }
 
 export default NoteLayout
